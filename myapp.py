5c33e89b21f51d64853fc3651ba9dc80b034263b
kpypp123 <kpypp123@gmail.com>
2026-05-12 22:28:21 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -251,7 +251,7 @@ if idx < len(current_list):
     if st.session_state.wrong_attempts >= 5:
         # 거의 정답에 가까운 힌트 (모음이나 자음 힌트 대신 아빠의 응원 메시지)
         st.toats("과연 정답은 무엇일까요?")
-        st.markdown(f"🔍 아빠의 특급 찬스 5**:정답은 **{ans[0]}...{ans[-1]}** 같은거야. 더 생각해봐)
+        st.markdown(f"🔍 아빠의 특급 찬스 5**:정답은 **{ans[0]}...{ans[-1]}** 같은거야. 더 생각해봐!")
 
     # 정답을 맞히기 전까지만 입력창을 보여줌
     if not st.session_state.correct_answer_given:
